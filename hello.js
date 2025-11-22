7454c002f07e700af77d5407cc65622c049ef210
Abhijeet Saini <abhijeetsaini@Abhijeets-MacBook-Pro-5.local>
2025-11-22 11:05:27 +0530

    Till CSS

---
+++ hello.js
@@ -0,0 +1,19 @@
+// taking variable
+var firstName="Randy"
+console.log(firstName)
+let lastName="Ortan"
+console.log(lastName)
+
+// var, let, const
+var a=5
+console.log(a)   // decalare
+    a=10
+console.log(a)   //re-assign
+
+var a=15
+console.log(a)   //re-declare
+
+
+let a=5
+console.log(a)
+
